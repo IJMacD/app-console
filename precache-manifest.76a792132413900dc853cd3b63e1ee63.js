009c174c3a33e5fcddc3c0fa9d87acb004866d0d
Iain MacDonald <IJMacD@gmail.com>
2020-09-10 01:17:12 +0800

    Updates

---
+++ precache-manifest.76a792132413900dc853cd3b63e1ee63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592efaf3901016261fd790fd4437c3b7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "51411d747ae1230b0fa9",
+    "url": "./static/css/main.5bad3d2b.chunk.css"
+  },
+  {
+    "revision": "966c9136a0058fb3c16a",
+    "url": "./static/js/2.6a2b2d64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6a2b2d64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51411d747ae1230b0fa9",
+    "url": "./static/js/main.8164d770.chunk.js"
+  },
+  {
+    "revision": "139d05dcac806d8842ef",
+    "url": "./static/js/runtime-main.1a31e048.js"
+  }
+]);
